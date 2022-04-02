2d9c6c229031cf37e0b29806ca4d4826398edd3a
Abid Adhikari <abidadhikari@gmail.com>
2022-04-02 13:14:33 +0545

    lab 8 completed

--- college/lab/lab8/abstractBaseClass.cpp
+++ college/lab/lab8/abstractBaseClass.cpp
@@ -9,7 +9,43 @@
 #include <iostream>
 using namespace std;
 
+class MEDIA{
+    public:
+    virtual float cost()=0;
+};
+
+class BOOK:public MEDIA{
+    int noOfPage;
+    int pricePerPage;
+    public:
+    void setValue(){
+        cout<<"Enter Number of Pages and price per page  : ";
+        cin>>noOfPage>>pricePerPage;
+    }
+    float cost(){
+        return (pricePerPage*noOfPage);
+    }
+};
+
+class AT:public MEDIA{
+    int timeInSecond;
+    int pricePerSecond;
+    public:
+        void setValue(){
+        cout<<"Enter Media Duration in Second and per second price : ";
+        cin>>timeInSecond>>pricePerSecond;
+    }
+    float cost(){
+        return (timeInSecond*pricePerSecond);
+    }
+};
 
 int main(){
+    BOOK b;
+    AT a;
+    b.setValue();
+    a.setValue();
+    cout<<"Price of book is "<<b.cost()<<endl;
+    cout<<"Price of audio tape is "<<a.cost()<<endl;
     return 0;
 }
--- college/lab/lab8/friendClass.cpp
+++ college/lab/lab8/friendClass.cpp
@@ -1,9 +1,35 @@
-// (1) Make use of a friend class to access private data members of another class. 
+// (1) Make use of a friend class to access private data members of another class.
 
 #include <iostream>
 using namespace std;
 
+class B;
 
-int main(){
+class A
+{
+    int num;
+    friend class B;
+
+public:
+    A()
+    {
+        num = 5;
+    }
+};
+
+class B
+{
+public:
+    void displayAFromB(A a)
+    {
+        cout << "Value of num in class A is : " << a.num << endl;
+    }
+};
+
+int main()
+{
+    A a;
+    B b;
+    b.displayAFromB(a);
     return 0;
 }
